9af72446e055b25b799f3ebfffa3960fceb3ba57
basker_ville <basker_ville@163.com>
2017-07-18 09:46:24 +0800

    twosum

---
+++ twosum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int ,int> hash;
+        vector<int> result;
+        int numberToFind;
+        for(int i = 0; i < nums.size();i++){
+            numberToFind = target - nums[i];
+            if(hash.find(numberToFind) != hash.end()){
+                result.push_back(hash[numberToFind] + 1);
+                result.push_back(i + 1);
+                return result;
+            }
+            hash[nums[i]] = i;
+        }
+        return result;    
+    }
+};
+int main(){
+    Solution s;
+    vector<int> key;
+    vector<int> nums;
+    int target = 9;
+    srand(0);
+    for(int i = 0;i < 9;i++){
+        nums.push_back(i);
+        printf("%d ",nums[i]);
+    }
+    key = s.twoSum(nums,target);
+    for(vector<int>::iterator iter = key.begin();iter != key.end();iter++){
+            printf("%d ",*iter);
+    }
+    return 0;
+
+}
